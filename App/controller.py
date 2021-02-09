b27058cd0fd0feaffd9451fe03892f3ead5af8fc
jsbautista <js.bautista@uniandes.edu.co>
2021-02-08 20:05:12 -0500

    Commit4b

--- App/controller.py
+++ App/controller.py
@@ -52,6 +52,7 @@ def loadTags(filename):
     return tags
 
 
-def loadBooksTags(catalog):
-    # TO-DO: Modificación de Est-1 y Est-2 en el Lab 2
-    pass
+def loadBooksTags(filename):
+    # TO-DO: Modificación del Est-1 y Est-2 en el Lab 2
+    btfile = cf.data_dir + filename
+    return model.addBookTags(btfile)
--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TO-DO: Modificación de Est-1 en el Lab 2
+    # TO-DO: Modificación del Est-1 en el Lab 2
     print("0- Salir")
 
 
@@ -63,7 +63,7 @@ def loadBookTags():
     """
     Cargar los Tags de libros
     """
-    # TO-DO: Modificación de Est-1 en el Lab 2
+    # TO-DO: Modificación del Est-1 en el Lab 2
 
 
 """
@@ -77,18 +77,21 @@ while True:
         books = loadBooks()
         print('Total de libros cargados: ' + str(lt.size(books)))
 
-        # TO-DO: Modificación de Est-1 en el Lab 2
+        # TO-DO: Modificación del Est-1 en el Lab 2
 
 
-        # TO-DO: Modificación de Est-2 en el Lab 2
+        # TO-DO: Modificación del Est-2 en el Lab 2
+        print('Ultimo libro cargado: ' + str(lt.lastElement(books)))
 
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    elif True:
-        # TO-DO: Modificación de Est-2 en el Lab 2
-        pass
+    elif int(inputs[0]) == 3:
+        # TO-DO: Modificación del Est-2 en el Lab 2
+        print("Cargando información de Book-Tags....")
+        booktags = loadBookTags()
+        print('Total de Book-Tags cargados: ' + str(lt.size(booktags)))
 
     else:
         sys.exit(0)
